659aa2bc3383da7342bcd30d77bc49dc579fb669
Banb4n <alban.such@gmail.com>
2017-10-09 11:03:35 +0200

    Add all files

---
+++ cart.php
@@ -0,0 +1,7 @@
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+      <?php print_r($_COOKIE['card']); ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
